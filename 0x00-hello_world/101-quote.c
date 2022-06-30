fb40c9942eafee40e7d47e6dd412c26ed26c1bec
Zerihun <znationabune@gmail.com>
2022-06-30 15:10:16 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<unistd.h>
+
+/**
+ * main - run all functions
+ * 
+ * Description: Change standard error message
+ * Return: Return 1 and exit
+ */
+int main(void)
+{
+	int a;
+	a =sizeof("and that piece of art is useful - Dora Korpar, 2015-10-19");
+	write(2,"and that piece of art is useful - Dora Korpar, 2015-10-19\n",a );
+	return (1);
+}
