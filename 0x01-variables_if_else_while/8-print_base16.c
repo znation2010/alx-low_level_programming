400c75f1d966521cb4381a9dbab3ad37e1ee9023
Zerihun <znationabune@gmail.com>
2022-07-01 19:31:14 -0700

    base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+/**
+ * main - print hexadecimal
+ * 
+ * Return: 0
+ */
+int main(void)
+{
+	char a = 'a';
+	char n = '0';
+
+	while (n <= '9')
+	{
+		putchar(n);									n++;
+											}
+	while (a <= 'f')
+	{	
+		putchar(a);									a++;
+											}
+	putchar('\n');
+	return (0);
+}
