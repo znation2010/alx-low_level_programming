46cace1899059233e8003a25ae858db9c5048253
znation2010 <56113884+znation2010@users.noreply.github.com>
2022-07-03 22:57:11 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -15,7 +15,7 @@ int main(void)
 	  sizeof("and that piece of art is useful - Dora Korpar, 2015-10-19");
 
 	write(2,
-	      'and that piece of art is useful" - Dora Korpar, 2015-10-19\n', l);
+	      "and that piece of art is useful - Dora Korpar, 2015-10-19\n", l);
 	return (1);
 }
 
