8b632465a1febf8db4f15db78203d6e8cbac11bd
znation2010 <56113884+znation2010@users.noreply.github.com>
2022-07-03 22:53:33 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,14 +3,19 @@
 
 /**
  * main - run all functions
- * 
+ *
  * Description: Change standard error message
  * Return: Return 1 and exit
  */
 int main(void)
 {
-	int a;
-	a =sizeof("and that piece of art is useful" - Dora Korpar, 2015-10-19");
-	write(2,"and that piece of art is useful" - Dora Korpar, 2015-10-19\n",a );
+	int l;
+
+	l =
+	  sizeof("and that piece of art is useful - Dora Korpar, 2015-10-19");
+
+	write(2,
+	      "and that piece of art is useful - Dora Korpar, 2015-10-19\n", l);
 	return (1);
 }
+
