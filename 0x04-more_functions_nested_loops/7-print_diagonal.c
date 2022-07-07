0e64a56c63c0748b3f214fa62afe785066eeede7
znation2010 <56113884+znation2010@users.noreply.github.com>
2022-07-07 22:20:07 +0300

    Update 7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,16 +1,12 @@
-#include "holberton.h"
-
+#include "main.h"
 /**
  * print_diagonal - check for a digit
  * @n : number of \\ to be printed
  * Return:void
  */
-
 void print_diagonal(int n)
 {
-
 	int i = 0, ii;
-
 	while (i < n && n > 0)
 	{
 		ii = 0;
@@ -19,12 +15,10 @@ void print_diagonal(int n)
 			_putchar(' ');
 			ii++;
 		}
-
 		_putchar('\\');
 		_putchar('\n');
 		i++;
 	}
 	if (i == 0)
 		_putchar('\n');
-
 }
