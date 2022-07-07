a5cb0ecaa499e2ce63a8352087c5e9c181599ce5
znation2010 <56113884+znation2010@users.noreply.github.com>
2022-07-07 22:16:46 +0300

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,16 +1,12 @@
-#include "holberton.h"
-
+#include "main.h"
 /**
  * more_numbers - prints more numbers
  * Return:void
  */
-
 void more_numbers(void)
 {
 	char n, c;
 	int i = 0;
-
-
 	while (i < 10)
 	{
 		for (n = 0; n <= 14; n++)
@@ -21,12 +17,9 @@ void more_numbers(void)
 				_putchar('1');
 				c = n % 10;
 			}
-
 			_putchar('0' + c);
 		}
-
 		_putchar('\n');
 		i++;
 	}
-
 }
