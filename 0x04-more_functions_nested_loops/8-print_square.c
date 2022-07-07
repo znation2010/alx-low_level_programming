82bf14b3f90061cefa9df4a62caee854b5b30fb3
znation2010 <56113884+znation2010@users.noreply.github.com>
2022-07-07 22:21:56 +0300

    Update 8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,16 +1,12 @@
-#include "holberton.h"
-
+#include "main.h"
 /**
  * print_square - check for a digit
  * @n : number of _ to be printed
  * Return:void
  */
-
 void print_square(int n)
 {
-
 	int i = 0, ii;
-
 	while (i < n && n > 0)
 	{
 		ii = 0;
@@ -19,11 +15,9 @@ void print_square(int n)
 			_putchar('#');
 			ii++;
 		}
-
 		_putchar('\n');
 		i++;
 	}
 	if (i == 0)
 		_putchar('\n');
-
 }
