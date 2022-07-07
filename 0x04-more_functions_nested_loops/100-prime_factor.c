e420a6885856ab699335ef23aa888cf36e2e64af
znation2010 <56113884+znation2010@users.noreply.github.com>
2022-07-07 22:25:10 +0300

    Update 100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,17 +1,14 @@
 #include <stdio.h>
-
 /**
  * main - finds and prints the largest prime factor of the number
  * 612852475143
  * Return: 0.
  */
-
 int main(void)
 {
 long num = 612852475143;
 long divisor = 2;
 long larg_prim = 0;
-
 while (num != 1)
 {
 if (num % divisor == 0)
